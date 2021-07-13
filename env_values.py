a83c0a763d702bd3fb30d9deaca63fc063a4ba42
lamh85 <lamh85@gmail.com>
2021-07-12 21:38:25 -0700

    Print environment values.

--- env_values.py
+++ env_values.py
@@ -2,4 +2,14 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-print(os.environ.get("TESTING_DUMMY_VARIABLE"))
+var_names = [
+  "TESTING_DUMMY_VARIABLE",
+  "DB_NAME",
+  "DB_HOST",
+  "DB_USER",
+  "DB_PASSWORD",
+  "DB_PORT"
+]
+
+for var_name in var_names:
+  print(os.environ.get(var_name))
